fe3c678ef1d73393dd36f36818ce0b96fbd8200a
WindClan <monstertoast2@gmail.com>
2024-07-29 00:42:55 -0400

    add my startup script

--- startup.lua
+++ startup.lua
@@ -1,2 +1,11 @@
-shell.setDir("/fwshop")
-shell.run("fwshop.lua")
+while true do
+    a = peripheral.find("monitor")
+    a.setTextScale(1)
+    shell.run("fwshop/fwshop.lua")
+    a.setBackgroundColor(colors.blue)
+    a.setTextColor(colors.white)
+    a.clear()
+    a.setCursorPos(1,1)
+    a.write("Offline! Restarting")
+    sleep(3)
+end
